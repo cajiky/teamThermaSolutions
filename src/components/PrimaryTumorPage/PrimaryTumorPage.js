7a8a097520f266251b35e8ee8d7e3fc887d51aa0
Christian Johannes <cajiky@gmail.com>
2018-12-17 14:58:41 -0600

    adjusted reducer to acomidate all dropdown options

--- src/components/PrimaryTumorPage/PrimaryTumorPage.js
+++ src/components/PrimaryTumorPage/PrimaryTumorPage.js
@@ -1,20 +1,94 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import moment from 'moment';
+
+const styles = theme => ({
+    gridItem:{
+        marginBottom: 30,
+    },
+});
+
 
 class PrimaryTumorPage extends Component {
+    state = {
+        dateOfPrimaryDiagnosis: '',
+        primaryLocation: '',
 
+    }
+
+    handleChange = (event) => {
+        this.setState ({
+            [event.target.name]: event.target.value,
+        })
+        console.log(this.state);
+    }
 
     render() {
+        const { classes } = this.props;
         return(
-            <div>
-                <h1>Primary Tumor Page</h1>
-
-                <h3>This is the First Tab in the Patient Profile</h3>
-                <h3>The pages includes input field and dropdowns for the following:</h3>
-                <h3>Primary Tumor Section, Treatment Primary, TNM Chart/Selection </h3>
-                
-            </div>
-
+            <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+            >
+                <Grid item xs={5}>
+                    <Grid
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="center"
+                    >
+                        <Grid item xs={12} className={classes.gridItem}>
+                            <Typography variant="h4">Prime Tumor</Typography>
+                        </Grid>
+                        <Grid
+                        container
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="center"
+                        >
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">Date of Primary Diagnosis:</Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <TextField 
+                                variant="outlined"
+                                type="date"
+                                fullWidth="true"
+                                onChange={this.handleChange}
+                                name="dateOfPrimaryDiagnosis"
+                                />
+                            </Grid>
+                            <Grid item xs={5} className={classes.gridItem}>
+                                <Typography variant="overline">Primary Location</Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                            
+                            </Grid>
+                    </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={5}>
+                    <Grid
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="center"
+                    style={{marginBottom: 30}}
+                    >
+                        <Grid item xs={12} className={classes.gridItem}>
+                            <Typography variant="h4">Treatment Prime Tumor</Typography>
+                        </Grid>
+                        
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
   
@@ -25,4 +99,4 @@ const mapStateToProps = reduxState => ({
 });
 
 
-export default connect(mapStateToProps) (PrimaryTumorPage)
+export default connect(mapStateToProps) (withStyles(styles)(PrimaryTumorPage))
