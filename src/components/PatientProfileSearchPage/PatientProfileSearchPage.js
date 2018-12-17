437aa6d7cf718930b3299c644e8bd70a909233b5
William Harrison <wm.harrison@gmail.com>
2018-12-17 12:07:31 -0600

    initial search components added, working on additional view

--- src/components/PatientProfileSearchPage/PatientProfileSearchPage.js
+++ src/components/PatientProfileSearchPage/PatientProfileSearchPage.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
 
 class PatientProfileSearchPage extends Component {
 
@@ -8,7 +13,20 @@ class PatientProfileSearchPage extends Component {
         return(
             <div>
                 <h1>Patient Profile/Search Page</h1>
-
+                <TextField
+                required
+                id="outlined-required"
+                label="Search for a Patient"
+                defaultValue="Patient ID e.x. 1234567890"
+                margin="normal"
+                variant="outlined"
+                />
+                <Button variant="contained" color="primary">
+                Search<SearchIcon/>
+                </Button>
+                <Button variant="contained" color="primary">
+                Add a Patient<AddIcon/>
+                </Button>
                 <h3>Here you will have a search field</h3>
                 <h3>This page will show patient profile information by id</h3>
                 <h3>You can also add a new patient by clicking "add patient" and filing out the information in the popup box</h3>
@@ -25,4 +43,4 @@ const mapStateToProps = reduxState => ({
 });
 
 
-export default connect(mapStateToProps) (PatientProfileSearchPage)
+export default connect(mapStateToProps)(PatientProfileSearchPage);
