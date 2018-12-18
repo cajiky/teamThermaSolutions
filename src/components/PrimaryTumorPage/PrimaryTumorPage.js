497f74d9bff75a327260054afd878909203235c1
Christian Johannes <cajiky@gmail.com>
2018-12-18 11:42:38 -0600

    updating the primary tumor page

--- src/components/PrimaryTumorPage/PrimaryTumorPage.js
+++ src/components/PrimaryTumorPage/PrimaryTumorPage.js
@@ -10,6 +10,11 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import MenuItem from '@material-ui/core/MenuItem';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+
 
 const styles = theme => ({
     gridItem:{
@@ -17,7 +22,13 @@ const styles = theme => ({
     },
     dropDown:{
         fullWidth: true,
-    }
+    },
+    formControl: {
+        margin: theme.spacing.unit * 3,
+      },
+      group: {
+        margin: `${theme.spacing.unit}px 0`,
+      },
 });
 
 
@@ -25,9 +36,9 @@ const styles = theme => ({
 
 class PrimaryTumorPage extends Component {
     state = {
-        dateOfPrimaryDiagnosis: '',
         primaryLocation: '',
-
+        mucinous: '',
+        // labelWidth: 105,
     }
 
     getDropDownOptions = () => {
@@ -52,7 +63,7 @@ class PrimaryTumorPage extends Component {
             container
             direction="row"
             justify="space-evenly"
-            alignItems="center"
+            alignItems="flex-start"
             >
                 <Grid item xs={5}>
                     <Grid
@@ -68,7 +79,7 @@ class PrimaryTumorPage extends Component {
                         container
                         direction="row"
                         justify="space-evenly"
-                        alignItems="center"
+                        alignItems="flex-start"
                         >
                             <Grid item xs={6} className={classes.gridItem}>
                                 <Typography variant="overline">Date of Primary Diagnosis:</Typography>
@@ -140,7 +151,7 @@ class PrimaryTumorPage extends Component {
                                 alignItems="center"
                                 className={classes.gridItem}
                                 > 
-                                    <Grid item xs={3}>
+                                    <Grid item xs={3} >
                                         <FormControl variant="outlined" fullWidth="true">
                                         <Select
                                             value={this.state.t}
@@ -161,14 +172,17 @@ class PrimaryTumorPage extends Component {
                                     </Grid>
                                     <Grid item xs={3}>
                                     <FormControl variant="outlined" fullWidth="true">
+                                    {/* <InputLabel ref={ref => {this.InputLabelRef = ref;}} htmlFor="m-input">Input Required</InputLabel> */}
+
                                         <Select
                                             value={this.state.m}
                                             onChange={this.handleChange}
+                                            labelWidth={this.state.labelWidth}
                                             input={
                                             <OutlinedInput
                                                 value={this.state.m}
                                                 name="m"
-                                                id="primary-location"
+                                                id="m-input"
                                             />
                                             }
                                         >
@@ -180,20 +194,14 @@ class PrimaryTumorPage extends Component {
                                     </Grid>
                                     <Grid item xs={3}>
                                     <FormControl variant="outlined" fullWidth="true">
-                                        <InputLabel
-                                            ref={ref => {
-                                            this.InputLabelRef = ref;
-                                            }}
-                                            htmlFor="primary-location"
-                                        >
-                                        Input Required
-                                        </InputLabel>
+                                    {/* <InputLabel ref={ref => {this.InputLabelRef = ref;}} htmlFor="primary-location">Input Required</InputLabel> */}
                                         <Select
                                             value={this.state.n}
                                             onChange={this.handleChange}
                                             input={
                                             <OutlinedInput
                                                 value={this.state.n}
+                                                labelWidth={this.state.labelWidth}
                                                 name="n"
                                                 id="primary-location"
                                             />
@@ -207,8 +215,8 @@ class PrimaryTumorPage extends Component {
                                     </Grid>
                                 </Grid>
                             </Grid>
-                            <Grid item xs={6}>
-                                <Typography variant="overline" className={classes.gridItem}>M Location:</Typography>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">M Location:</Typography>
                             </Grid>
                             <Grid item xs={6} className={classes.gridItem}>
                                 <FormControl variant="outlined" fullWidth="true">
@@ -232,19 +240,19 @@ class PrimaryTumorPage extends Component {
                                     </Select>
                                 </FormControl>
                             </Grid>
-                            <Grid item xs={6}>
-                                <Typography variant="overline" className={classes.gridItem}>Differentiation:</Typography>    
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">Differentiation:</Typography>    
                             </Grid>
-                            <Grid item xs={6}>
+                            <Grid item xs={6} className={classes.gridItem}>
                                 <FormControl variant="outlined" fullWidth="true" required>
                                     <Select
-                                        value={this.state.mLocation}
+                                        value={this.state.differentiation}
                                         onChange={this.handleChange}
                                         input={
                                         <OutlinedInput
-                                            value={this.state.mLocation}
-                                            name="mLocation"
-                                            id="primary-location"
+                                            value={this.state.differentiation}
+                                            name="differentiation"
+                                            id="differentiation"
                                         />
                                         }
                                     >
@@ -255,6 +263,23 @@ class PrimaryTumorPage extends Component {
                                             <MenuItem key={dif.id} value={dif.id}>{dif.status}</MenuItem> 
                                         ))} 
                                     </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControl component="fieldset" className={classes.formControl}>
+                                <FormLabel component="legend">Mucinous</FormLabel>
+                                    <RadioGroup
+                                        aria-label="mucinous"
+                                        name="mucinous"
+                                        className={classes.group}
+                                        value={this.state.mucinous}
+                                        onChange={this.handleChange}
+                                    >
+                                        <FormControlLabel value='1' control={<Radio />} label="Yes" />
+                                        <FormControlLabel value='0' control={<Radio />} label="No" />
+                                        <FormControlLabel value='null' control={<Radio />} label="Unknown" />
+
+                                    </RadioGroup>
                                 </FormControl>
                             </Grid>
                         </Grid>
@@ -271,7 +296,180 @@ class PrimaryTumorPage extends Component {
                         <Grid item xs={12} className={classes.gridItem}>
                             <Typography variant="h4">Treatment Prime Tumor</Typography>
                         </Grid>
-                        
+                        <Grid
+                        container
+                        direction="row"
+                        justify="space-evenly"
+                        alignItems="flex-start"
+                        >
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline" >
+                                    Date of Primary Surgery:
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <TextField 
+                                variant="outlined"
+                                type="date"
+                                fullWidth="true"
+                                onChange={this.handleChange}
+                                name="dateOfPrimarySurgery"
+                                value={this.state.dateOfPrimarySurgery}
+                                />
+                            </Grid >
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">
+                                    Intervention Type:
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <FormControl variant="outlined" fullWidth="true">
+                                    <Select
+                                        value={this.state.interventionType}
+                                        onChange={this.handleChange}
+                                        input={
+                                        <OutlinedInput
+                                            value={this.state.interventionType}
+                                            name="interventionType"
+                                            id="interventionType"
+                                        />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {this.props.reduxState.dropdownOptions.interventionTypeOptions.map(int => (
+                                            <MenuItem key={int.id} value={int.id}>{int.status}</MenuItem> 
+                                        ))} 
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">
+                                    Setting:        
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <FormControl variant="outlined" fullWidth="true">
+                                    <Select
+                                        value={this.state.setting}
+                                        onChange={this.handleChange}
+                                        input={
+                                        <OutlinedInput
+                                            value={this.state.setting}
+                                            name="setting"
+                                            id="setting"
+                                        />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {this.props.reduxState.dropdownOptions.interventionTypeOptions.map(set => (
+                                            <MenuItem key={set.id} value={set.id}>{set.status}</MenuItem> 
+                                        ))} 
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <Typography variant="overline">
+                                    Prime Tumor Surgery:
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem}>
+                                <FormControl variant="outlined" fullWidth="true">
+                                    <Select
+                                        value={this.state.primaryTumorSurgery}
+                                        onChange={this.handleChange}
+                                        input={
+                                        <OutlinedInput
+                                            value={this.state.primaryTumorSurgery}
+                                            name="primaryTumorSurgery"
+                                            id="primaryTumorSurgery"
+                                        />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {this.props.reduxState.dropdownOptions.primaryTumorSurgeryOptions.map(pts => (
+                                            <MenuItem key={pts.id} value={pts.id}>{pts.status}</MenuItem> 
+                                        ))} 
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} className={classes.gridItem}>
+                                <FormControl component="fieldset" className={classes.formControl}>
+                                <FormLabel component="legend">Adj Chemotherapy</FormLabel>
+                                    <RadioGroup
+                                        aria-label="adj"
+                                        name="adj"
+                                        className={classes.group}
+                                        value={this.state.adj}
+                                        onChange={this.handleChange}
+                                    >
+                                        <FormControlLabel value='true' control={<Radio />} label="Yes" />
+                                        <FormControlLabel value='false' control={<Radio />} label="No" />
+                                        <FormControlLabel value='null' control={<Radio />} label="Unknown" />
+
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem} align="center">
+                                <Typography variant="overline">
+                                    Type
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem} align="center">
+                                <Typography variant="overline">
+                                    Biological
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem} align="center">
+                                <FormControl variant="outlined" style={{width: 200}}>
+                                    <Select
+                                        value={this.state.type}
+                                        onChange={this.handleChange}
+                                        input={
+                                        <OutlinedInput
+                                            value={this.state.type}
+                                            name="type"
+                                            id="type"
+                                        />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {this.props.reduxState.dropdownOptions.adjChemoTypeOptions.map(adj => (
+                                            <MenuItem key={adj.id} value={adj.id}>{adj.status}</MenuItem> 
+                                        ))} 
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6} className={classes.gridItem} align="center">
+                                <FormControl variant="outlined" style={{width: 200}}>
+                                    <Select
+                                        value={this.state.biological}
+                                        onChange={this.handleChange}
+                                        input={
+                                        <OutlinedInput
+                                            value={this.state.biological}
+                                            name="biological"
+                                            id="biological"
+                                        />
+                                        }
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        {this.props.reduxState.dropdownOptions.biologicalOptions.map(adj => (
+                                            <MenuItem key={adj.id} value={adj.id}>{adj.status}</MenuItem> 
+                                        ))} 
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
