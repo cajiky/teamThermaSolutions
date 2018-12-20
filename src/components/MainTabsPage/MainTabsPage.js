18dbea495583ebe8659d79d32e618b231067fc3a
William Harrison <wm.harrison@gmail.com>
2018-12-20 14:55:47 -0600

    working on the persistent patient info section component that will sit on top of the tabs

--- src/components/MainTabsPage/MainTabsPage.js
+++ src/components/MainTabsPage/MainTabsPage.js
@@ -20,6 +20,7 @@ import PostOpPage from '../PostOpPage/PostOpPage';
 import FollowUpPage from '../FollowUpPage/FollowUpPage';
 import AdditionalDataPage from '../AdditionalDataPage/AdditionalDataPage';
 import ManageUsersPage from '../ManageUsersPage/ManageUsersPage';
+import CurrentPatientInfo from '../CurrentPatientInfo/CurrentPatientInfo';
 
 function TabContainer(props) {
     return (
@@ -47,6 +48,7 @@ state = {
   componentDidMount () {
     this.props.dispatch({type: 'GET_DROPDOWN_OPTIONS'})
     this.props.dispatch({type: 'FETCH_POST_OP'});
+    this.props.dispatch({type: 'TEST_PATIENT'});
   }
 
   handleTabChange = (event, value) => {
@@ -58,8 +60,9 @@ state = {
         const { value } = this.state;
         return(
             <div className={classes.root}>
-                <h1>Main Tabs Page</h1>
-                <h3>This Page houses all the Tab Components</h3>
+                {/* <h1>Main Tabs Page</h1>
+                <h3>This Page houses all the Tab Components</h3> */}
+                {this.props.reduxState.patientReducer.patient ? (<CurrentPatientInfo/>) : (<></>)}
                 <AppBar position="static" color="default">
                     <Tabs
                     value={value}
