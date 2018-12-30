5638eb6d6b37d412d347d2bbb8289a064fb5273f
RhondaStark92 <rhondastark@comcast.net>
2018-12-30 12:47:02 -0600

    clean up adverse_events_saga code

--- server/routes/adverse_event.router.js
+++ server/routes/adverse_event.router.js
@@ -50,22 +50,17 @@ router.post('/',rejectUnauthenticated, async (req, res) => {
 //
 // GET ROUTER TO RETRIEVE ADVERSE EVENTS FOR PATIENT ID
 router.get('/:id', rejectUnauthenticated, (req, res) => {
-//   console.log('query.id', req.query.id);
-
-// select event_options.id, postop_id, clavian_score from event_options 
-// left outer join (select * from events where postop_id = 1) as foo on event_options.id = foo.event_id
-// order by event_options.id
-
-//   const queryText = 'SELECT * FROM events WHERE postop_id=$1';
-    const queryText = `SELECT event_options.name, event_options.id as id, 
-                        selected_events.checked, selected_events.postop_id, 
-                        selected_events.clavien_score
-                        FROM event_options
-                        LEFT OUTER JOIN (
-                            SELECT *, true as checked FROM adverse_events
-                            WHERE postop_id = $1) AS selected_events
-                        ON event_options.id = selected_events.event_options_id
-                        ORDER BY event_options.sort`
+  
+  // get any adverse_events and all event_options
+  const queryText = `SELECT event_options.name, event_options.id as id, 
+                    selected_events.checked, selected_events.postop_id, 
+                    selected_events.clavien_score
+                    FROM event_options
+                    LEFT OUTER JOIN (
+                        SELECT *, true as checked FROM adverse_events
+                        WHERE postop_id = $1) AS selected_events
+                    ON event_options.id = selected_events.event_options_id
+                    ORDER BY event_options.sort`
 
     pool.query(queryText, [req.params.id])      
       .then(results => res.send(results.rows))
@@ -74,70 +69,5 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
           res.sendStatus(500);
       });
 });
-
-
-// POST ROUTER TO ADD UPDATE ADVERSE EVENTS
-// this will remove any existing and update with new
-// router.post('/', rejectUnauthenticated, (req, res) => {
-//   const newAdverseEvent = req.body;
-//   const queryText = `INSERT INTO adverse_events 
-//   ("postop_id", "event_options_id", "clavien_score") 
-//     SELECT * FROM UNNEST(($1)::int[],($2)::int[],($3)::int[])`;
-//   const queryValues = [
-//     newAdverseEvent.arrayPostOpIds,
-//     newAdverseEvent.arrayEventOptionIds,
-//     newAdverseEvent.arrayClavienScores,
-//   ];
-//   // console.log('sql query for new items for new user', queryText, queryValues);
-//   pool.query(queryText, queryValues)
-//     .then(() => { res.sendStatus(201); })
-//     .catch((err) => {
-//       console.log('Error completing INSERT item query', err);
-//       res.sendStatus(500);
-//     });
-// });
-
-// router.put('/', rejectUnauthenticated, (req, res) => {
-//     const id = req.body.id
-//     const icu_stays = req.body.icu_stays
-//     const mcu_stays = req.body.mcu_stays;
-//     const hospital_stays = req.body.hospital_stays;
-//     const notes = req.body.notes;
-//     const serious_advese_event = req.body.serious_advese_event;
-//     const score = req.body.score;
-//     const reoperation = req.body.reoperation;
-//     const hospital_mortality = req.body.hospital_mortality;
-//     const status_at_discharge = req.body.status_at_discharge;
-//     const discharge_notes = req.body.discharge_notes;
-
-//     const queryText = `UPDATE postop 
-//         SET icu_stays=$2, mcu_stays=$3, hospital_stays=$4,
-//         notes=$5, serious_advese_event=$6, score=$7,
-//         reoperation=$8, hospital_mortality=$9,
-//         status_at_discharge=$10, discharge_notes=$11
-//         WHERE id=$1`;
-
-//     pool.query(queryText, [id, icu_stays, mcu_stays, hospital_stays,
-//         notes, serious_advese_event, score, reoperation, hospital_mortality,
-//         status_at_discharge, discharge_notes])
-//       .then((result) => { res.send(result.rows); })
-//       .catch((err) => {
-//         console.log('Error completing UPDATE post op query', err);
-//         res.sendStatus(500);
-//       });            
-// });
-
-// DELETE ROUTER FOR ITEM
-router.delete('/', rejectUnauthenticated, (req, res) => {
-  console.log('in delete on server', req.query);
-  const queryText = 'DELETE FROM adverse_events WHERE postop_id=$1';
-  pool.query(queryText, [req.query.id])
-    .then(() => { res.sendStatus(200); })
-    .catch((err) => {
-      console.log('Error completing DELETE adverse events query', err);
-      res.sendStatus(500);
-    });
-});
-
 
 module.exports = router;
