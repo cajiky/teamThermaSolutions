48111533456dc52d9697882c59d3a9c501347f9f
William Harrison <wm.harrison@gmail.com>
2019-01-08 16:06:44 -0600

    updated cookie code block, working on async after add patient

--- src/components/MainTabsPage/MainTabsPage.js
+++ src/components/MainTabsPage/MainTabsPage.js
@@ -44,15 +44,16 @@ state = {
     value: 0,
   };
 
-  getPatientIDFromCookie = () => {
-    console.log(document.cookie.replace(/(?:(?:^|.*;\s*)patientID\s*\=\s*([^;]*).*$)|^.*$/, "$1"))
-    this.props.dispatch({type:'GET_PATIENT_ID_FROM_COOKIE', payload: document.cookie.replace(/(?:(?:^|.*;\s*)patientID\s*\=\s*([^;]*).*$)|^.*$/, "$1")})
-  }
+  // getPatientIDFromCookie = () => {
+  //   console.log(document.cookie.replace(/(?:(?:^|.*;\s*)patientID\s*\=\s*([^;]*).*$)|^.*$/, "$1"))
+  //   this.props.dispatch({type:'GET_PATIENT_ID_FROM_COOKIE', payload: document.cookie.replace(/(?:(?:^|.*;\s*)patientID\s*\=\s*([^;]*).*$)|^.*$/, "$1")})
+  // }
 
   componentDidMount () {    
     // console.log('in main tabs page for patient#', this.props.reduxState.patientReducer.patient.id);
-    this.getPatientIDFromCookie()
+    // this.getPatientIDFromCookie(patientId)
     let patientId = document.cookie.replace(/(?:(?:^|.*;\s*)patientID\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    this.props.dispatch({type:'GET_PATIENT_ID_FROM_COOKIE', payload: patientId})
     this.props.dispatch({type: 'GET_DROPDOWN_OPTIONS'});
     this.props.dispatch({type: 'FETCH_POST_OP', payload: patientId});
     this.props.dispatch({type: 'FETCH_ADVERSE_EVENT', payload: patientId});
