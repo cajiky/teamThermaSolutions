31828549339d27ef8b43bc5a967ddb819f996668
Micah Johnson <micahj2005@hotmail.com>
2019-01-15 14:30:15 -0600

    added theme files

---
+++ src/components/App/ColorectalTheme.js
@@ -0,0 +1,33 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const appTheme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0849eb',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#789dfa',
+    },
+    background: {
+      paper: "#fff",
+      default: "#fff"
+    },
+    // error: will use the default color
+  },
+  typography: {
+    useNextVariants: true,
+    fontSize: 16,
+  },
+  overrides: {
+    MuiExpansionPanelSummary: { // Name of the component ⚛️ / style sheet
+      root: { // Name of the rule
+        // backgroundColor: '#888888', // Some CSS
+      },
+    },
+  },
+});
+
+export default appTheme;
