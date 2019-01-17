0001aa3fc10258250b442ad52b508456d819d9b6
RhondaStark92 <rhondastark@comcast.net>
2019-01-17 09:02:11 -0600

    push build

---
+++ build/precache-manifest.95c5e206560a7a3f7c93c38bece1a4e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd908215377e8001ba8f",
+    "url": "/static/css/main.11e25031.chunk.css"
+  },
+  {
+    "revision": "dd908215377e8001ba8f",
+    "url": "/static/js/main.dd908215.chunk.js"
+  },
+  {
+    "revision": "029505c2c8e311f3f314",
+    "url": "/static/css/1.8ef4df99.chunk.css"
+  },
+  {
+    "revision": "029505c2c8e311f3f314",
+    "url": "/static/js/1.029505c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "669a1a4aac04fee88312a3d589f22573",
+    "url": "/index.html"
+  }
+];
